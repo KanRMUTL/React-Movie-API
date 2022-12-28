057aaef3263bf504fa777a3744f0cc3c56a2ba80
Kiadtisak Lowongsa <kanlowongsa20@gmail.com>
2022-12-28 21:43:55 +0700

    simple counter by  redux toolkit

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,8 @@
 import {configureStore} from '@reduxjs/toolkit'
+import counterReducer from '../features/counter/counterSlice'
 
 export const store = configureStore({
-    reducer: {}
+    reducer: {counter: counterReducer}
 })
 
 export type RootState = ReturnType<typeof store.getState>
---
+++ src/features/counter/Counter.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import type { RootState } from "../../app/store";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./counterSlice";
+
+function Counter() {
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <div>
+        <div>
+          <button
+            aria-label="Increment value"
+            onClick={() => dispatch(increment())}
+          >
+            Increment
+          </button>
+          <span>{count}</span>
+          <button
+            aria-label="Decrement value"
+            onClick={() => dispatch(decrement())}
+          >
+            Decrement
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
